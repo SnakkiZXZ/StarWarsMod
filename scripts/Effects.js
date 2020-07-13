b018a50112681d4414aedfc15a5b7c3d4c5bc9a8
SnakkiZXZ <databagrusrus4567654@Mail.ru>
2020-07-13 16:41:27 +0500

    Effects remote

--- scripts/Effects.js
+++ scripts/Effects.js
@@ -6,14 +6,14 @@ module.exports = {
         return chargeHealth = newEffect(30, e => {
     
 Draw.color(color1, color2, e.fin());
-    rand = new Rand()
-    rand.setSeed(e.id)
+    rand = new Rand();
+    rand.setSeed(e.id);
     var foutpow = 1-Interpolation.pow3In.apply(e.fin())
     
     for(i=0; i<int; i++){
         
-    x = rand.random(-15, 15)
-    y = rand.random(-15, 15)
+    x = rand.random(-15, 15);
+    y = rand.random(-15, 15);
     Fill.circle(e.x+x*foutpow, e.y+y*foutpow, 2*e.fin())
     
  }})
